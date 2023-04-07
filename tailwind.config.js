9717f43d83be2bd1ea14dab40b48fcf7c063b610
pontakornth <pontakorn61@gmail.com>
2023-04-07 21:27:44 +0700

    Install Tailwind and DaisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    // Remove other themes to simplify development
+    themes: ['light']
+  }
+}
+
