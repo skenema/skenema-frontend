a181efb0dad0474ba27fe4f772f357dd0294bf99
TaninDean <reiodean.dean@gmail.com>
2023-04-08 10:25:14 +0700

    Add Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-[8vh] text-center text-4xl w-full bg-[#771616]'>
+        Navbar
+    </div>
+  )
+}
+
+export default Navbar
