542f5519684193d101fc613758e99eaa2ba0247f
pontakornth <pontakorn61@gmail.com>
2023-04-12 15:29:43 +0700

    Fetch movies API

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -2,13 +2,23 @@ import { useEffect, useState } from "react"
 import MovieCard from "../components/MovieCard";
 import { movies } from "../mocks/consts";
 import { Movie } from "../mocks/types"
+import useSWR from 'swr'
+import { fetchJSON } from "../utils";
 
 const Movie = () => {
-    const [data, setData] = useState<Movie[]>(movies);
+    // const [data, setData] = useState<Movie[]>(movies);
+    const { data, error } = useSWR<Movie[]>('/api/movies', fetchJSON)
+    if (error) return (
+        <div className="w-full h-full grid place-content-center overflow-scroll">
+            <h1 className="text-error font-bold text-xl">Error</h1>
+            <p>{error.toString()}</p>
+        </div>
+    )
 
     return (
         <div className="w-full h-full grid place-content-center overflow-scroll">
-            {data.map((movie, id) => {
+            {!data && <p>Loading...</p>}
+            {data && data.map((movie, id) => {
                 return <MovieCard key={movie.id} movieDetail={movie}/>
             })}
         </div>
