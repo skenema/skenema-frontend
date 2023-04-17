02fc19a081fe1cd63a3110712fe5eee88c6df01b
pontakornth <pontakorn61@gmail.com>
2023-04-17 21:23:08 +0700

    Add QR Code generation

--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -1,16 +1,18 @@
+import QRCode from "react-qr-code"
 import { getCustomDateFormat } from "../utils"
 
 interface props {
     title: string
     seat: number
     showtime: string
+    ticketId: number
 }
 
-const Ticket = ({title, seat, showtime}: props) => {
+const Ticket = ({title, seat, showtime, ticketId}: props) => {
   return (
     <div className="w-full h-[20vh] p-3 grid grid-cols-[20%,80%] bg-[#D9D9D9] gap-10">
         <div className="w-full h-full">
-            <div className="bg-black w-full h-full"></div>
+          <QRCode style={{maxWidth: "100%", height: "auto", width: "100%"}} value={`${import.meta.env.VITE_BASE_URL}/validate-ticket/${ticketId}`} />
         </div>
         <div className="grid items-center">
             <p className="text-3xl">{title} - Seat {seat}</p>
--- src/components/TicketSheet.tsx
+++ src/components/TicketSheet.tsx
@@ -11,7 +11,7 @@ const TicketSheet = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
     return (
         <div ref={ref}>
             {props.seats.map(seat => (
-                <Ticket title={props.title} seat={seat} showtime={props.showtime} />
+                <Ticket ticketId={seat} title={props.title} seat={seat} showtime={props.showtime} />
             ))}
         </div>
     )
