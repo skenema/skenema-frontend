3fc0f645b8fc9c24492129c396400d52a960f109
pontakornth <pontakorn61@gmail.com>
2023-04-19 20:48:45 +0700

    Add access token handling as atom

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,32 @@
-export function fetchWithAuth(url: string, accessToken: string, init?: RequestInit | undefined) {
-  return fetch(url , {
+import { atom } from "jotai";
+
+export function fetchWithAuth(
+  url: string,
+  accessToken: string,
+  init?: RequestInit | undefined
+) {
+  return fetch(url, {
     headers: {
-      Authorization: `Bearer ${accessToken}`
+      Authorization: `Bearer ${accessToken}`,
     },
-    ...init
-  })
+    ...init,
+  });
 }
+
+/*
+I use atom because I don't want to handle context. It is also kinda simpler to understand.
+- Pontakorn Paesaeng
+*/
+const _authAtom = atom(localStorage.getItem("accessToken") ?? null);
+
+export const authAtom = atom(
+  (get) => get(_authAtom),
+  (get, set, newToken: string | null) => {
+    set(_authAtom, newToken);
+    if (newToken) {
+      localStorage.setItem("accessToken", newToken);
+    } else {
+      localStorage.removeItem("accessToken");
+    }
+  }
+);
