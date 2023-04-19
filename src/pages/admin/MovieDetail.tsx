a891781df5b55a654b3b87408d9b70e4fe266d1e
pontakornth <pontakorn61@gmail.com>
2023-04-19 21:25:29 +0700

    Use API for showtime

--- src/pages/admin/MovieDetail.tsx
+++ src/pages/admin/MovieDetail.tsx
@@ -1,18 +1,19 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getSeatBackgroundColor } from "../Reservation";
 import useSWR from "swr";
-import { fetchJSON } from "../../utils";
-import { Movie } from "../../types/responses";
+import { fetchJSON, getCustomDateFormat } from "../../utils";
+import { Movie, Showtime } from "../../types/responses";
 const MovieDetail = () => {
   const params = useParams();
   const movieId = params.movieId;
   // No 404 handling on the routing part due to time limit
-  const { data: movie, error } = useSWR<Movie>(`/api/movies/${movieId}`, fetchJSON);
-  if (error) {
+  const { data: movie, error: movieError } = useSWR<Movie>(`/api/movies/${movieId}`, fetchJSON);
+  const { data: showtimes, error: showtimeError} = useSWR<Showtime[]>(`/api/reservation/${movieId}`, fetchJSON)
+  if (movieError) {
     return (
       <div className="w-1/2 mx-auto mt-8">
         <h1 className="text-4xl font-bold text-error">
-          Error: {error.toString()}
+          Error: {movieError.toString()}
         </h1>
       </div>
     );
@@ -27,11 +28,15 @@ const MovieDetail = () => {
           <p className="text-lg">Cinema {movie.cinema}</p>
 
           <h2 className="text-3xl font-bold">Showtimes</h2>
+          {showtimeError && <p className="text-error">{showtimeError.toString()}</p>}
+          {(!showtimeError && !showtimes) && <p>Loading...</p>}
           <ul>
-            <li>
-              29 September 2023 - 16:00{" "}
+          {showtimes && showtimes.map(showtime => (
+            <li key={showtime.id}>
+              {getCustomDateFormat(new Date(showtime.start_time))}{" "}
               <button className="btn btn-info">Detail</button>
             </li>
+          ))}
           </ul>
 
           <div className="flex justify-end gap-8">
