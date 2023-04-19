218f18a076e99d44217f0d10bc03a094ae98ba24
pontakornth <pontakorn61@gmail.com>
2023-04-19 21:31:57 +0700

    Add login for convenience

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,17 @@
+import { useAtom } from 'jotai'
 import React from 'react'
+import { authAtom } from '../auth'
 
 const Navbar = () => {
+  const [accessToken, setAccessToken] = useAtom(authAtom)
   return (
     <div className="navbar bg-base-200">
-      <a href='/' className="btn btn-ghost normal-case text-xl">Home</a>
+      <div className='navbar-start'>
+        <a href='/' className="btn btn-ghost normal-case text-xl">Home</a>
+      </div>
+      <div className='navbar-end'>
+        <a className='btn btn-ghost normal-case text-xl' href={accessToken ? '/logout' : '/login'}>{accessToken ? 'Logout' : 'Login to admin system'}</a>
+      </div>
     </div>
   )
 }
