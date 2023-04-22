50a50ebafd024d488669dd0fcdb4a4aad5d8f5b2
pontakornth <pontakorn61@gmail.com>
2023-04-22 17:48:04 +0700

    stop using mock server

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,23 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     server: {
-      port: 4000
-    }
+      port: 4000,
+      proxy: {
+        "/api/movies": {
+          target: "http://127.0.0.1:8989/movies_service/movies",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/movies/, ""),
+        },
+        "/api/auth": { target: "http://127.0.0.1:9000", changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/auth/, "")
+       },
+        "/api/reservation": {target: "http://127.0.0.1:8000/reservation_service/reservation" , changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/reservation/, "")
+      },
+        "/api/ticket": {target: "http://localhost:8888/ticket_service", changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api\/ticket/, "")
+      },
+      },
+    },
   };
 });
