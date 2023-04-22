28f3d25b857eea669b9d3ef4306bf24b6ea85d69
pontakornth <pontakorn61@gmail.com>
2023-04-22 18:06:58 +0700

    Fix incorrect button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         </a>
       </div>
       <div className="navbar-end">
-        {accessToken ? (
+        {!accessToken ? (
           <a href="/login" className="btn btn-ghost normal-case text-xl">
             Login
           </a>
